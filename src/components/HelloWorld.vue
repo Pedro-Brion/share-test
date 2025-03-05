d84588b46e2409df181fa5eb5a295e4d6defe24d
Pedro Brion <pedro.brion@gmail.com>
2025-03-05 13:25:43 -0300

    add html image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,42 @@
 <script setup>
+import { toBlob } from "html-to-image";
 import { ref } from "vue";
-const count = ref(0);
+const text = ref(
+  "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
+);
+
+const node = ref();
 
 async function share() {
   try {
-    const res = await fetch("/Agrimens-logo-2.png");
-    const blob = await res.blob();
-    console.log(blob);
-    const file = new File(
-      [blob],
-      `${blob.type.split("/")[0]}.${blob.type.split("/")[1]}`,
-      { type: blob.type }
-    );
+    const res = await toBlob(node.value);
+    console.log(res);
+    const img = new File([res], "filename.png", { type: res.type });
+    console.log(img);
     navigator.share({
       title: "Test",
       text: "Aqui vai minha pontuação no simulado agrinvest, tente você também",
       url: "/teste/?text=test",
-      files: [file],
+      files: [img],
     });
   } catch (err) {
-    error.value = err;
     console.error(err);
   }
 }
 </script>
 
 <template>
-  <img height="250px" src="/Agrimens-logo-2.png" alt="" />
+  <div class="card-test" ref="node">
+    <img height="250px" src="/Agrimens-logo-2.png" alt="" />
+    <div>
+      <h2>Aqui vai o texto</h2>
+      <p class="text">{{ text }}</p>
+    </div>
+  </div>
   <div class="card">
     <button type="button" @click="share">Share</button>
   </div>
+  <input v-model="text" type="text" />
 </template>
 
 <style scoped>
@@ -40,5 +47,19 @@ img {
 }
 img:hover {
   border-color: #646cff;
+}
+
+.card-test {
+  display: flex;
+  gap: 18px;
+  padding: 12px;
+  border-radius: 14px;
+  border: 2px solid white;
+  background-color: #3c3040;
+}
+
+.text {
+  width: 250px;
+  text-align: justify;
 }
 </style>
